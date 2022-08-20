872348849a29b5717462c65fb5bc5d4244c1797c
Pavel Halko <pavel@finnovate.io>
2022-08-20 01:54:19 -0400

    more unneeded code

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import Buttonc from './components/Buttonc';
 import { Button } from 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
-  const test = () => {
-    console.log("this work?");}
   return (
     <div className="App">
-    <Buttonc title = 'title' funct = {test} color = 'red'/>
     </div>
   );
 }
