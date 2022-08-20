ab92b298f720cb32875a7ce367d200d5e7831c5c
Pavel Halko <pavel@finnovate.io>
2022-08-20 17:05:18 -0400

    Added linear regression

--- src/algorithms/analysis.js
+++ src/algorithms/analysis.js
@@ -5,6 +5,33 @@ export const linearRegression = (xPoints, yPoints) => {
     distance = 0
     for (let i = 0; i < xPoints.length; i++) {
         distance = xPoints[i] - xPoints[0];
-        totalX += yPoints[i]
+        totalX += xPoints[i]
+        totalY += yPoints[i]
     }
+
+    xAvg = totalX / xPoints.length
+    yAvg = totalY / yPoints.length
+
+
+    numerator = 0
+    denominator = 0
+    for (let i = 0; i < xPoints.length; i++) {
+        tempX = xPoints[i] - xAvg
+        tempY = yPoints[i] - yAvg
+        denominator = tempX * tempX
+        numerator = tempX * tempY
+    }
+    
+    mValue = numerator / denominator
+    bValue = yAvg - (mValue * xAvg)
+
+    final = []
+    for (let i = 0; i < xPoints.length; i++) {
+        temp = {}
+        temp.x = xPoints[i]
+        temp.y = (mValue * xPoints[i]) + bValue
+        final.push(temp)
+    }
+
+    return final
 }
